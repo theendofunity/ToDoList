ff074b4851a74d19ef82ff1fcf479d79ed2be324
Dmitrii Dudkin <ddudkin@findmykids.org>
2025-03-02 13:48:37 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by ddudkin on 2.3.25..
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
